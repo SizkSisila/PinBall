c3b9f544e6bbf5ae8d71778c7999c12be9fd1751
SizkSisila <77679655+SizkSisila@users.noreply.github.com>
2021-02-07 19:08:05 +0900

    Initial commit

---
+++ Assets/BallController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+    // ボールが見える可能性のあるz軸の最大値
+    private float visiblePosZ = -6.5f;
+
+    // スコア関連
+    private float Score;                    // 現在のスコア
+    private float SmallStarPoint = 10f;     // SmallStarの得点
+    private float LargeStarPoint = 20f;     // LargeStarの得点
+    private float SmallCloudPoint = 30f;    // SmallCloudの得点
+    private float LargeCloudPoint = 50f;    // LargeCloudの得点
+    
+    // ゲームオーバーを表示するテキスト
+    private GameObject gameoverText;
+
+    // スコアを表示するテキスト
+    private GameObject ScoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // シーン中のGameOverTextオブジェクトを取得
+        this.gameoverText = GameObject.Find("GameOverText");
+        this.ScoreText = GameObject.Find("ScoreText");
+
+        // スコアを初期化
+        this.Score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ボールが画面外に出た場合
+        if(this.transform.position.z < this.visiblePosZ)
+        {
+            //GameoverTextにゲームオーバーを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+    }
+
+    // オブジェクトとの衝突時の処理
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            // 衝突相手がSmallStarの場合
+            case "SmallStarTag":
+                this.Score += this.SmallStarPoint;  // 得点加算
+                this.ScoreText.GetComponent<Text>().text = "Score:" + this.Score; // UIを更新
+                break;
+            // 衝突相手がLargeStarの場合
+            case "LargeStarTag":
+                this.Score += this.LargeStarPoint;  // 得点加算
+                this.ScoreText.GetComponent<Text>().text = "Score:" + this.Score; // UIを更新
+                break;
+            case "SmallCloudTag":
+                this.Score += this.SmallCloudPoint;  // 得点加算
+                this.ScoreText.GetComponent<Text>().text = "Score:" + this.Score; // UIを更新
+                break;
+            case "LargeCloudTag":
+                this.Score += this.LargeCloudPoint;  // 得点加算
+                this.ScoreText.GetComponent<Text>().text = "Score:" + this.Score; // UIを更新
+                break;
+        }
+    }
+}
---
+++ Assets/FripperController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour
+{
+    // HingeJointコンポーネントを入れる
+    private HingeJoint myHingeJoint;
+
+    // 初期の傾き
+    private float defaultAngle = 20;
+
+    //弾いた時の傾き
+    private float flickAngle = -20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // HingeJointコンポーネント宇徳
+        this.myHingeJoint = GetComponent<HingeJoint>();
+
+        // フリッパーの傾きを設定
+        SetAngle(this.defaultAngle);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 左フリッパーを動かす
+        if ((Input.GetKeyDown(KeyCode.LeftArrow)    // 左矢印キーを押したとき
+             || Input.GetKeyDown(KeyCode.A)         // Aを押したとき
+             || Input.GetKeyDown(KeyCode.S)          // Sを押したとき
+             || ( Input.GetMouseButton(0) && Input.mousePosition.x < Screen.width / 2) )   // 画面右側をクリック(タップ)した時
+             && tag == "LeftFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+
+        // 右フリッパーを動かす
+        if((Input.GetKeyDown(KeyCode.RightArrow)    // 右矢印キーを押したとき
+             || Input.GetKeyDown(KeyCode.D)         // Dを押したとき
+             || Input.GetKeyDown(KeyCode.S)         // Sを押したとき
+             || ( Input.GetMouseButton(0) && Input.mousePosition.x >= Screen.width / 2) )    // 画面左側をクリック(タップ)した時
+            && tag == "RightFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+
+        // キーが離されたときフリッパーを元に戻す
+        if ((Input.GetKeyUp(KeyCode.LeftArrow)
+             || Input.GetKeyUp(KeyCode.A)         // Aを押したとき
+             || Input.GetKeyUp(KeyCode.S)         // Sを押したとき
+             || Input.GetMouseButtonUp(0))        // クリック(タップ)を離したとき
+             && tag == "LeftFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+        if((Input.GetKeyUp(KeyCode.RightArrow)
+             || Input.GetKeyUp(KeyCode.D)         // Dを押したとき
+             || Input.GetKeyUp(KeyCode.S)         // Sを押したとき
+             || Input.GetMouseButtonUp(0))        // クリック(タップ)を離したとき
+             && tag == "RightFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+    }
+
+    public void SetAngle(float angle)
+    {
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+}
